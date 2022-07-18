fd322e29c6b812c9e51772954c7b21599b1e7ac5
Sergeypervak <har4enko1983@gmail.com>
2022-07-18 20:25:56 +0300

    Effect start

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import {State} from './State'
+import {Effect} from './Effect'
+
 
 function App() {
   return (
 <div className='App'>
   <State/>
+  <Effect/>
 </div>
 
 
---
+++ src/Effect.jsx
@@ -0,0 +1,18 @@
+import React, {useEffect} from "react";
+
+
+
+
+
+
+
+
+export const Effect = () => {
+
+
+
+return <div>
+
+</div>
+
+};
