281c0582ae5e1e12dca4b57acfcaa08a78f8264e
Sergeypervak <har4enko1983@gmail.com>
2022-07-18 07:21:08 +0300

    initialValue useState

--- src/State.jsx
+++ src/State.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 
 export const State = () => {
-const [value, setValue] = useState(0);
+const [value, setValue] = useState(() => {
+    const userCount = localStorage.getItem('count')
+    return +userCount || 0;
+});
 
 setValue((prevValue)  => {
   return prevValue + 1;  
