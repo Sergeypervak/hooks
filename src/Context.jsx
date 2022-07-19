87fdb321f7504585624d77eb566839c30d46d843
Sergeypervak <har4enko1983@gmail.com>
2022-07-19 20:09:52 +0300

    context progress

--- src/Context.jsx
+++ src/Context.jsx
@@ -4,5 +4,23 @@ import React, {createContext, useState} from "react";
 export const CustomContext = createContext();
 
 export const Context = (props) => {
-   const [] = useState(); 
+   const [books, setBooks] = useState([
+    {id: 1, title: 'JS'},
+    {id: 2, title: 'React'},
+    {id: 3, title: 'NodeJS'},
+   ]); 
+
+
+const addBook = (book) => {
+    setBooks([book, ...books]) 
+    }
+
+const removeBook = (id) =>  {
+    setBooks(books.filter(book => book.id !==id )) 
+}   
+
+return <CustomContext.Provider>
+    {props.children}
+</CustomContext.Provider>
+
 }
